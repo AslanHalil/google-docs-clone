6781efca4c04f540653de05220209e1fc3f44fcf
Aslan Halil <AslanCHalil@outlook.com>
2024-12-26 14:15:33 +0200

    Added database support by setting up database through Convex.

--- docs-tutorial/src/app/(home)/page.tsx
+++ docs-tutorial/src/app/(home)/page.tsx
@@ -1,16 +1,29 @@
-import Link from "next/link";
+"use client"
 
 import {Navbar} from "./navbar";
 import {TemplatesGallery} from "@/app/(home)/templates-gallery";
+import {useQuery} from "convex/react";
+import {api} from "../../../convex/_generated/api";
 
 const Home = () => {
+    const documents = useQuery(api.documents.get);
+
+    if (documents === undefined) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return (
         <div className="flex min-h-screen flex flex-col">
             <div className="fixes top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-                <Navbar />
+                <Navbar/>
             </div>
             <div className="mt-16">
                 <TemplatesGallery/>
+                {documents?.map((document) => (
+                    <span key={document._id}>{document.title}</span>
+                ))}
             </div>
         </div>
     )
