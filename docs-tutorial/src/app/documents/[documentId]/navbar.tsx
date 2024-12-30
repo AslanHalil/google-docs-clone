408692314fd9cc070f67a2c5bb5cde614e00b9aa
Aslan Halil <AslanCHalil@outlook.com>
2024-12-30 22:19:39 +0200

    Implemented notification feature.

--- docs-tutorial/src/app/documents/[documentId]/navbar.tsx
+++ docs-tutorial/src/app/documents/[documentId]/navbar.tsx
@@ -29,6 +29,7 @@ import {BsFilePdf} from "react-icons/bs";
 import {useEditorStore} from "@/store/use-editor-store";
 import {OrganizationSwitcher, UserButton} from "@clerk/nextjs";
 import {Avatars} from './avatars';
+import {Inbox} from "./inbox";
 
 export const Navbar = () => {
 
@@ -215,6 +216,7 @@ export const Navbar = () => {
             </div>
             <div className="flex gap-3 items-center">
                 <Avatars/>
+                <Inbox/>
                 <OrganizationSwitcher
                     afterCreateOrganizationUrl="/"
                     afterLeaveOrganizationUrl="/"
