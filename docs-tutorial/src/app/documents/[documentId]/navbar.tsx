a522a937d8276fee09e75df47e313912c45da536
Aslan Halil <AslanCHalil@outlook.com>
2024-12-30 19:43:20 +0200

    Populate user info when people are in room.

--- docs-tutorial/src/app/documents/[documentId]/navbar.tsx
+++ docs-tutorial/src/app/documents/[documentId]/navbar.tsx
@@ -28,6 +28,7 @@ import {
 import {BsFilePdf} from "react-icons/bs";
 import {useEditorStore} from "@/store/use-editor-store";
 import {OrganizationSwitcher, UserButton} from "@clerk/nextjs";
+import {Avatars} from './avatars';
 
 export const Navbar = () => {
 
@@ -213,6 +214,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center">
+                <Avatars/>
                 <OrganizationSwitcher
                     afterCreateOrganizationUrl="/"
                     afterLeaveOrganizationUrl="/"
